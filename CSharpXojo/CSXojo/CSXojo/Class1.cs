fc4592980f7744f040142bb6cb1d9601ee3e3122
Eugene Dakin <eugenedakin@users.noreply.github.com>
2020-09-02 20:23:10 -0600

    Add files via upload
    Created Xojo and CSharp files for the project

---
+++ CSharpXojo/CSXojo/CSXojo/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RGiesecke.DllExport;
+
+namespace CSXojo
+{
+    public class Class1
+    {
+        [DllExport]
+        public static int AddTwo(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
